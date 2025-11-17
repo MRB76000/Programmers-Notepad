f00fbf9d266af0bdeb0b0f159aa21c101b71fecc
MRB76000 <mb840705@gmail.com>
2025-11-16 23:28:51 -0500

    we can now look at the current cursor position

--- main.py
+++ main.py
@@ -2,15 +2,17 @@ from tkinter import *
 from tkinter import ttk
 
 
-
-def edit(event):
-    box.config(state="disabled")  
-    box.config(cursor="plus")
-def insert(event):
-    box.config(state="normal")  
-def select(event):
+def select():
     box.tag_add("hello","1.00", "1.10")
     box.tag_config("hello", background="white")
+
+def edit(event):
+    select()
+    box.config(state="disabled")  
+    box.config(cursor="plus")
+    print(box.index('current'))
+def insert(event):
+    box.config(state="normal")  
 root = Tk()
 
 root.title("Notebook")
@@ -23,7 +25,6 @@ box.pack(padx = 5, pady = 5)
 
 
 root.bind('<Escape>', edit)
-root.bind('<Escape>', select)
 root.bind('<i>', insert)
 
 root.mainloop()
